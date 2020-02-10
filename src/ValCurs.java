12f75d99b53d0b6d866d3ae1b5394dc44e3ca681
maslus <zykova.ls@ya.ru>
2020-02-10 15:48:23 +0300

    step 1

---
+++ src/ValCurs.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public class ValCurs {
+
+    LocalDate Date;
+    String name;
+
+    List<Valute> valuteList;
+}
---
+++ src/Valute.java
@@ -0,0 +1,10 @@
+public class Valute {
+
+    String ID;
+
+    String NunCode;
+    String CharCode;
+    short Nominal;
+    String Name;
+    float Value;
+}
